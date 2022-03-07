a5c0cad521132dd16e20499927b5f660fcb03404
SylwiaSobota <32569088+SylwiaSobota@users.noreply.github.com>
2022-03-07 11:30:42 +0100

    Create dodajPyt_e13.php

---
+++ app/dodajPyt_e13.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" type="image/x-icon" href="./rysunki/comp.ico">   
+    <meta charset="utf-8">
+    <title>Baza pytań egzaminu zawodowego teoretycznego</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <style> @import url("./style/styl.css"); </style> 
+<?php
+
+$pytanie = $_POST['pytanie'];
+$odpA =$_POST['odpA'];
+$odpB =$_POST['odpB'];
+$odpC =$_POST['odpC'];
+$odpD =$_POST['odpD'];
+$obraz = $_POST['obrazek'];
+$poprawna = $_POST['poprawna'];
+
+//echo $pytanie."  ".$odpA."   ";
+$polacz = mysqli_connect('localhost', 'root', '', 'pytania');
+mysqli_set_charset($polacz, "utf8");
+
+$sql = "INSERT INTO e13 (nrPyt, pyt, odpA, odpB, odpC, odpD, obraz, poprawna) VALUES (null, '$pytanie', '$odpA', '$odpB', '$odpC','$odpD','$obraz','$poprawna')";
+if (mysqli_query($polacz, $sql)) {
+    echo "Dodano nowy rekord <br>";
+    echo "<button class='back'><a href='./dodaj_e13.html'>Dodaj kolejne pytanie</a></button>";
+    echo "<button class='back'><a href='./przeglad_e13.php'>Zobacz wszystkie pytania</a></button>";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($polacz);
+    echo "<button class='back'><a href='./dodaj_e13.html'>Powrót do dodawania pytania</a></button>";
+  }
+
+
+?>
+</head>
+</html>
