0c21cb71aef3220f68bab4df3814491fbd6c0142
SylwiaSobota <32569088+SylwiaSobota@users.noreply.github.com>
2022-03-07 11:29:57 +0100

    Create admin.php

---
+++ app/admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" type="image/x-icon" href="./rysunki/comp.ico">   
+        <meta charset="utf-8">
+        <title>Baza pytań egzaminu zawodowego teoretycznego</title>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <style> @import url("./style/styl.css"); </style> 
+    </head>
+    <body>
+    <a href="index.php">Wyloguj</a>
+    <header>&copy Sylwia Sobota </header>
+        <h1>Panel administratora dla zawodu technik informatyk</h1>
+        
+        <label><a href="./przeglad_e12.php">Przedgląd pytań kwalifikacji E12</a></label>
+        <label><a href="./dodaj_e12.html">Dodaj pytania do E12</a></label><br>
+        <label><a href="./przeglad_e13.php">Przedgląd pytań kwalifikacji E13</a></label>
+        <label><a href="./dodaj_e13.html">Dodaj pytania do E13</a></label><br>
+        <label><a href="./przeglad_e14.php">Przedgląd pytań kwalifikacji E14</a></label>
+        <label><a href="./dodaj_e14.html">Dodaj pytania do E14</a></label><br>
+        <label><a href="./przeglad_ee08.php">Przedgląd pytań kwalifikacji EE08</a></label>
+        <label><a href="./dodaj_ee08.html">Dodaj pytania do EE08</a></label><br>
+        <label><a href="./przeglad_ee09.php">Przedgląd pytań kwalifikacji EE09</a></label>
+        <label><a href="./dodaj_ee09.html">Dodaj pytania do EE09</a></label><br>
+        <label><a href="./uczniowie.php">Przegląd uczniów</a></label><br>
+        <label><a href="./administratorzy.php">Przegląd adminów</a></label><br>
+
+
+
+    </body>
+</html>
