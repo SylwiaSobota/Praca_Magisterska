707a935fe9b7ab5cca3a4b957ccb75e1d712ef02
SylwiaSobota <32569088+SylwiaSobota@users.noreply.github.com>
2022-03-07 11:45:32 +0100

    Create edytuj_e14.php

---
+++ app/edytuj_e14.php
@@ -0,0 +1,56 @@
+<?php
+if(isset($_GET['nrPyt'])){
+    $nrPyt = $_GET['nrPyt'];
+}
+$polacz = mysqli_connect('localhost', 'root', '', 'pytania');
+mysqli_set_charset($polacz, "utf8");
+$sql1 = "SELECT * FROM e14 WHERE nrPyt=$nrPyt;";
+$dane = mysqli_query($polacz, $sql1);
+$edyt = mysqli_fetch_array($dane);
+
+
+?>
+<html>
+    <head>
+        <link rel="icon" type="image/x-icon" href="./rysunki/comp.ico">   
+        <meta charset="utf-8">
+        <title>Baza pytań egzaminu zawodowego teoretycznego</title>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <style> @import url("./style/styl.css"); </style> 
+    </head>
+    <body>
+        <form method="post" action="przeslij_e14.php?nrPyt=<?=$edyt['nrPyt']?>" accept-charset="utf-8">
+            <table id="edytuj">
+                <tr >
+                    <td>Pytanie</td><td><input type="text" name="pyt" value="<?=$edyt['pyt']?>" ></td>
+                </tr>
+
+                <tr>
+                    <td>Odpowiedź A</td><td><input type="text" name="odpA" value="<?=$edyt['odpA']?>"></td>
+                </tr>
+                <tr>
+                    <td>Odpowiedź B</td><td><input type="text" name="odpB" value="<?=$edyt['odpB']?>"></td>
+                </tr>
+                <tr>
+                    <td>Odpowiedź C</td><td><input type="text" name="odpC" value="<?=$edyt['odpC']?>"></td>
+                </tr>
+                <tr>
+                    <td>Odpowiedź D</td><td><input type="text" name="odpD" value="<?=$edyt['odpD']?>"></td>
+                </tr>
+                <tr>
+                    <td>Ścieżka do obrazka</td><td><input type="text" name="obraz" value="<?=$edyt['obraz']?>"></td>
+                </tr>
+                <tr>
+                    <td>Poprawna</td><td><input type="text" name="poprawna" value="<?=$edyt['poprawna']?>" maxlength="1"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" name="zapisz" value="Zapisz" class="info"></td>
+                </tr>
+            </table>
+        </form>
+        <a href="przeglad_e14.php"><button class="back">Wróć do przeglądu pytań</button></a>
+        <?php 
+        mysqli_close($polacz);
+        ?>
+    </body>
+</html>
